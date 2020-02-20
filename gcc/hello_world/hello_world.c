03c54c71c534ce39749f190c4c2da798b99cc602
Jiang Xiaohui <jiang_xiaohui@163.com>
2020-02-20 20:15:56 +0800

    hello_world of c

---
+++ gcc/hello_world/hello_world.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+        > File Name: print_triangle_number.c
+      > Author: Xiaohui Jiang
+      > Mail: jiang_xiaohui@163.com 
+      > Created Time: Thu Feb 20 18:37:53 2020
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+  printf("hello world\n");
+  return 0;
+}
