432b1af49a23f3467d2ef0006b065e241d6b4bda
Jiang Xiaohui <jiang_xiaohui@163.com>
2020-03-12 21:56:03 +0800

    some exercise in book

--- gcc/hello_world/print_number.c
+++ gcc/hello_world/print_number.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include <limits.h>
 
 void print_2(char val)
 {
@@ -16,15 +17,15 @@ void bin_out(void* p_i, size_t len)
     unsigned char * p_c = (char*)p_i;
     printf("\nbase 10:");
     for (int i = 0; i < len; i++){
-        printf("%d ", *(p_c+i));
+	printf("%d ", *(p_c+i));
     }
     printf("\nbase 16:");
     for (int i = 0; i < len; i++){
-        printf("0x%02x ", *(p_c+i));
+	printf("0x%02x ", *(p_c+i));
     }
     printf("\nbase  2:");
     for (int i = 0; i < len; i++){
-        print_2(*(p_c+i));
+	print_2(*(p_c+i));
     }
     printf("\n");
 }
@@ -32,37 +33,40 @@ void bin_out(void* p_i, size_t len)
 
 int main()
 {
-  int   a = 8/5;
-  float b = 8.0/5;
-  double c = 8.0/5;
+    int   a = 8/5;
+    float b = 8.0/5;
+    double c = 8.0/5;
 
-  printf("\nhello int:\n");
-  printf("%d\n", a);
-  printf("%e\n", a);
-  printf("%f\n", a);
-  printf("%lf\n", a);
-  printf("%.1lf\n", a);
-  printf("%.2lf\n", a);
-  bin_out(&a, sizeof(int));
+    printf("\nhello int:\n");
+    printf("%d\n", a);
+    printf("%e\n", a);
+    printf("%f\n", a);
+    printf("%lf\n", a);
+    printf("%.1lf\n", a);
+    printf("%.2lf\n", a);
+    bin_out(&a, sizeof(int));
 
-  printf("\nhello float:\n");
-  printf("%d\n", b);
-  printf("%e\n", b);
-  printf("%f\n", b);
-  printf("%lf\n", b);
-  printf("%.1lf\n", b);
-  printf("%.2lf\n", b);
-  bin_out(&b, sizeof(float));
+    printf("\nhello float:\n");
+    printf("%d\n", b);
+    printf("%e\n", b);
+    printf("%f\n", b);
+    printf("%lf\n", b);
+    printf("%.1lf\n", b);
+    printf("%.2lf\n", b);
+    bin_out(&b, sizeof(float));
 
-  printf("\nhello double:\n");
-  printf("%d\n", c);
-  printf("%e\n", c);
-  printf("%f\n", c);
-  printf("%lf\n", c);
-  printf("%.1lf\n", c);
-  printf("%.2lf\n", c);
-  bin_out(&c, sizeof(double));
+    printf("\nhello double:\n");
+    printf("%d\n", c);
+    printf("%e\n", c);
+    printf("%f\n", c);
+    printf("%lf\n", c);
+    printf("%.1lf\n", c);
+    printf("%.2lf\n", c);
+    bin_out(&c, sizeof(double));
 
+    printf("%d\n", 11111 * 11111);
+    printf("%d\n", INT_MAX);
+    printf("%d\n", INT_MIN);
 
-  return 0;
+    return 0;
 }
